676df9ce6d5173b4be56cd46fc502b94c4e3b145
traletav <traletav@yandex.ru>
2020-06-19 22:57:47 +0300

    update main.js

--- gulpfile.js
+++ gulpfile.js
@@ -9,13 +9,13 @@ var gulp = require('gulp'),
 
     gulp.task('sass', function(){
         return gulp.src('app/scss/**/*.scss')
-        .pipe(sass({outputStyle: 'compressed'}))
-        .pipe(rename({suffix :'.min'}))
-        .pipe(autoprefixer({
-         overrideBrowserslist: ['last 8 version']
-        }))
-        .pipe(gulp.dest('app/css'))
-        .pipe(browserSync.reload({stream: true}))
+                .pipe(sass({outputStyle: 'compressed'}))
+                .pipe(rename({suffix :'.min'}))
+                .pipe(autoprefixer({
+                 overrideBrowserslist: ['last 8 version']
+                }))
+                .pipe(gulp.dest('app/css'))
+                .pipe(browserSync.reload({stream: true}))
     });
 
     gulp.task('style', function(){
@@ -59,7 +59,7 @@ var gulp = require('gulp'),
     });
 
     gulp.task('watch', function(){
-        gulp.watch('app/scss/style.scss', gulp.parallel('sass'))
+        gulp.watch('app/scss/**/*.scss', gulp.parallel('sass'))
         gulp.watch('app/*.html', gulp.parallel('html'))
         gulp.watch('app/js/*.js', gulp.parallel('js'))
     });
